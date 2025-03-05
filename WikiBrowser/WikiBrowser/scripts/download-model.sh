cee45734f5957b4798e4ea02549949a592e41021
Mohammed Aashif <163309073+aashif000@users.noreply.github.com>
2025-03-05 16:50:07 +0530

    Delete WikiBrowser/WikiBrowser directory

--- WikiBrowser/WikiBrowser/scripts/download-model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create models directory if it doesn't exist
-mkdir -p client/public/models
-
-# Download DistilGPT-2 model
-curl -L https://huggingface.co/distilgpt2/resolve/main/model.onnx -o client/public/models/distilgpt2-model.onnx
-
-echo "Model downloaded successfully"
